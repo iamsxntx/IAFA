bc7e62f92a7d3858b99c6e76ea9b041b6ebf55d5
iamsxntx <daleonpa@unal.edu.co>
2025-02-18 22:17:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")  # Habilita WebSockets con CORS
