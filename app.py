b55c3018a7ac767f8fae665b38ead95c42b2d0f7
iamsxntx <daleonpa@unal.edu.co>
2025-02-24 06:56:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
-from flask_cors import CORS  # Agrega esta línea
+from flask_cors import CORS  
 
 app = Flask(__name__)
 CORS(app)  # Habilita CORS en toda la app
